9952862c7cb4e96ded56e5c25c5409aa55b25ec4
Prajwal-PL <prajwalpl096@gmail.com>
2024-05-03 17:58:55 +0530

    init

---
+++ server/server.ts
@@ -0,0 +1,10 @@
+import express from "express";
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("hello world!");
+});
+
+app.listen(4000, () => {
+  console.log("server running on port:4000");
+});
